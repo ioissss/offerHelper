2b5543c38ca6fd8a8c52554e39f500d023123653
ioissss <91249397+ioissss@users.noreply.github.com>
2025-01-16 20:38:19 +0800

    1
    1

--- jobHelper/miniprogram/pages/index/index.js
+++ jobHelper/miniprogram/pages/index/index.js
@@ -96,6 +96,76 @@ this.setData({active_tab:Number(e.currentTarget.dataset.index)});
   goto(){
     this.setData({showAddPanel:true});
   },
+  afterRead(event) {
+    const { file } = event.detail;
+    var fileList = [];
+    fileList.push({
+      url:file.url,
+      name:"descImg",
+      isImage:true,
+      deletable:true
+    });
+    this.setData({fileList});
+    // 当设置 mutiple 为 true 时, file 为数组格式，否则为对象格式
+    // wx.uploadFile({
+    //   url: 'https://example.weixin.qq.com/upload', // 仅为示例，非真实的接口地址
+    //   filePath: file.url,
+    //   name: 'file',
+    //   formData: { user: 'test' },
+    //   success(res) {
+    //     // 上传完成需要更新 fileList
+    //     const { fileList = [] } = this.data;
+    //     fileList.push({ ...file, url: res.data });
+    //     this.setData({ fileList });
+    //   },
+    // });
+  },
+  deleteImg(event){
+    const index = event.detail.index;
+    var fileList = this.data.fileList;
+    fileList.splice(index,1);
+    this.setData({fileList});
+  },
+  
+  // --------------- 上传文件 -----------------
+  isDocument(file) {
+    // 获取文件路径的后缀名
+    const supportedDocumentTypes = ['pdf', 'doc', 'docx', 'txt', 'xls', 'xlsx', 'ppt', 'pptx']; // 支持的文档类型
+    const fileExtension = file.split('.').pop().toLowerCase(); // 提取后缀名并转为小写
+  
+    // 判断文件类型是否在支持的文档类型中
+    return supportedDocumentTypes.includes(fileExtension);
+  },
+  chooseFileFromMessage(){
+    console.log("选择文件");
+    wx.chooseMessageFile({
+      count: 1,
+      success:(e)=>{
+        const files = e.tempFiles;
+        const filename = files[0].name;
+        const filePath = files[0].path;
+        const fileType = files[0].type;
+        if(!this.isDocument(filePath) || fileType !== "file"){
+          wx.showToast({
+            title: '不支持的文件类型',
+            icon:'error'
+          });
+          return;
+        }
+        var resumeFile = {
+          name:filename,
+          path:filePath
+        };
+        this.setData({resumeFile});
+      },
+      fail:(e)=>{
+        wx.showToast({
+          title: '上传文件失败',
+          icon:'error'
+        })
+      }
+    })
+  },
 
   /**
    * 生命周期函数--监听页面加载
